c497a5e76e4545003b6a662655bbf9a67d8e7d28
deletescape <deletescape@outlook.com>
2017-02-27 11:02:19 +0100

    Add option to directly specify remote URL when initializing MetaSpace

--- src/main/java/ch/deletescape/metaspace/InitCommand.java
+++ src/main/java/ch/deletescape/metaspace/InitCommand.java
@@ -3,10 +3,14 @@ package ch.deletescape.metaspace;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.Properties;
 
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.RemoteAddCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.transport.URIish;
 
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
@@ -17,17 +21,25 @@ import ch.deletescape.metaspace.printer.Printer;
 public class InitCommand {
   @Parameter(names = { "-n", "--name" }, description = "Give your MetaSpace a name")
   private String name = "my_metaspace";
+  @Parameter(names = "--remote", description = "Specify the main remote git repository to be used")
+  private String remote;
   private Properties metaProperties = new Properties();
 
   public void run() {
     metaProperties.setProperty("metaspace.name", name);
     metaProperties.setProperty("metaspace.creationdatetime", String.valueOf(System.currentTimeMillis()));
     try {
-      Git.init().call();
+      Git git = Git.init().call();
       FileWriter fw = new FileWriter(new File(".metaspace"));
       metaProperties.store(fw, null);
+      if (remote != null) {
+        RemoteAddCommand remoteAdd = git.remoteAdd();
+        remoteAdd.setName(Constants.DEFAULT_REMOTE_NAME);
+        remoteAdd.setUri(new URIish(remote));
+        remoteAdd.call();
+      }
       Printer.out.println("Successfully initialized MetaSpace \"%s\"", name);
-    } catch (GitAPIException | IOException e) {
+    } catch (GitAPIException | IOException | URISyntaxException e) {
       Printer.err.println(e.toString());
     }
   }
