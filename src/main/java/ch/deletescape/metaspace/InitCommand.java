bfb22434d6c706539360041286fa48aa3230dc31
deletescape <deletescape@outlook.com>
2017-02-27 10:05:15 +0100

    Save some information into a file when creating a MetaSpace

--- src/main/java/ch/deletescape/metaspace/InitCommand.java
+++ src/main/java/ch/deletescape/metaspace/InitCommand.java
@@ -1,20 +1,34 @@
 package ch.deletescape.metaspace;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 
+import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
 
 import ch.deletescape.metaspace.printer.Printer;
 
 @Parameters(commandDescription = "Initialize a new MetaSpace")
 public class InitCommand {
+  @Parameter(names = { "-n", "--name" }, description = "Give your MetaSpace a name")
+  private String name = "my_metaspace";
+  private Properties metaProperties = new Properties();
+
   public void run() {
+    metaProperties.setProperty("metaspace.name", name);
+    metaProperties.setProperty("metaspace.creationdatetime", String.valueOf(System.currentTimeMillis()));
     try {
       Git.init().call();
-    } catch (GitAPIException e) {
+      FileWriter fw = new FileWriter(new File(".metaspace"));
+      metaProperties.store(fw, null);
+      Printer.out.println("Successfully initialized MetaSpace \"%s\"", name);
+    } catch (GitAPIException | IOException e) {
       Printer.err.println(e.toString());
     }
-    Printer.out.println("Successfully initialized MetaSpace");
   }
 }
